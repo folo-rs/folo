75aeb0dab05c03d8df7941404496a9a7cd327553
Sander Saares <sander@saares.eu>
2025-01-28 11:14:03 +0200

    Revert "fix"
    This reverts commit affba2c0947ca072797d29c1970fe1feeca231fa.

--- crates/folo_hw/benches/effects_of_memory.rs
+++ crates/folo_hw/benches/effects_of_memory.rs
@@ -16,7 +16,7 @@ use folo_hw::ProcessorSet;
 use frozen_collections::{FzHashMap, FzScalarMap, MapQuery};
 use itertools::Itertools;
 use nonempty::nonempty;
-use spin::{barrier::Barrier, relax::Loop};
+use spin::{barrier::Barrier, Loop};
 
 // https://github.com/cloudhead/nonempty/issues/68
 extern crate alloc;
