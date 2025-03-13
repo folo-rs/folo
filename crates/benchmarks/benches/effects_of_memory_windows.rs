83aef3de80a7a7557dce3965d56aab4c741caa22
Sander Saares <sander@saares.eu>
2025-03-13 07:35:16 +0200

    AllocPerMemoryRegionHeap

--- crates/benchmarks/benches/effects_of_memory_windows.rs
+++ crates/benchmarks/benches/effects_of_memory_windows.rs
@@ -16,8 +16,14 @@ mod not_windows {
 }
 
 mod windows {
+    use std::{
+        cell::RefCell,
+        sync::{Arc, Mutex},
+    };
+
     use criterion::Criterion;
     use itertools::Itertools;
+    use many_cpus::{HardwareInfo, HardwareTracker};
     use many_cpus_benchmarking::{Payload, WorkDistribution, execute_runs};
     use windows::Win32::{
         Foundation::HANDLE,
@@ -34,6 +40,7 @@ mod windows {
         execute_runs::<AllocDefaultHeap, 10_000>(c, WorkDistribution::all());
         execute_runs::<AllocPerThreadHeap, 10_000>(c, WorkDistribution::all());
         execute_runs::<AllocPerThreadHeapThreadSafe, 10_000>(c, WorkDistribution::all());
+        execute_runs::<AllocPerMemoryRegionHeap, 10_000>(c, WorkDistribution::all());
     }
 
     const CHUNK_SIZE: usize = 1024; // 1 KB
@@ -116,23 +123,42 @@ mod windows {
         }
     }
 
-    thread_local!(static PER_THREAD_HEAP_THREAD_SAFE: CustomHeap = CustomHeap::new(true));
-    thread_local!(static PER_THREAD_HEAP: CustomHeap = CustomHeap::new(false));
+    /// Allocates and frees memory on the a custom per-thread thread-safe Windows heap.
+    #[derive(Debug, Default)]
+    struct AllocPerMemoryRegionHeap {}
 
-    struct CustomHeap {
+    impl Payload for AllocPerMemoryRegionHeap {
+        fn new_pair() -> (Self, Self) {
+            (Self::default(), Self::default())
+        }
+
+        fn prepare(&mut self) {}
+
+        fn process(&mut self) {
+            PER_REGION_HEAP.with(|heap| {
+                let chunks = (0..CHUNK_COUNT)
+                    .map(|_| heap.alloc(CHUNK_SIZE))
+                    .collect_vec();
+
+                for chunk in chunks {
+                    heap.free(chunk);
+                }
+            });
+        }
+    }
+
+    thread_local!(static PER_THREAD_HEAP_THREAD_SAFE: ThreadSafeCustomHeap = ThreadSafeCustomHeap::new());
+    thread_local!(static PER_THREAD_HEAP: SingleThreadedCustomHeap = SingleThreadedCustomHeap::new());
+    linked::instance_per_thread!(static PER_REGION_HEAP: MemoryRegionSpecificHeap = MemoryRegionSpecificHeap::new());
+
+    struct SingleThreadedCustomHeap {
         heap: HANDLE,
     }
 
-    impl CustomHeap {
-        pub fn new(thread_safe: bool) -> Self {
-            let flags = if thread_safe {
-                HEAP_FLAGS(0)
-            } else {
-                HEAP_NO_SERIALIZE
-            };
-
+    impl SingleThreadedCustomHeap {
+        pub fn new() -> Self {
             Self {
-                heap: unsafe { HeapCreate(flags, 0, 0) }.unwrap(),
+                heap: unsafe { HeapCreate(HEAP_NO_SERIALIZE, 0, 0) }.unwrap(),
             }
         }
 
@@ -145,9 +171,96 @@ mod windows {
         }
     }
 
-    impl Drop for CustomHeap {
+    impl Drop for SingleThreadedCustomHeap {
         fn drop(&mut self) {
             unsafe { HeapDestroy(self.heap) }.unwrap();
+        }
+    }
+
+    struct ThreadSafeCustomHeap {
+        heap: HANDLE,
+    }
+
+    impl ThreadSafeCustomHeap {
+        pub fn new() -> Self {
+            Self {
+                heap: unsafe { HeapCreate(HEAP_FLAGS(0), 0, 0) }.unwrap(),
+            }
+        }
+
+        pub fn alloc(&self, size: usize) -> *mut std::ffi::c_void {
+            unsafe { HeapAlloc(self.heap, HEAP_FLAGS(0), size) }
+        }
+
+        pub fn free(&self, ptr: *mut std::ffi::c_void) {
+            unsafe { HeapFree(self.heap, HEAP_FLAGS(0), Some(ptr)) }.unwrap();
+        }
+    }
+
+    impl Drop for ThreadSafeCustomHeap {
+        fn drop(&mut self) {
+            unsafe { HeapDestroy(self.heap) }.unwrap();
+        }
+    }
+
+    // SAFETY: The HANDLE is not thread-safe by type but is logically thread-safe in this case.
+    unsafe impl Send for ThreadSafeCustomHeap {}
+    // SAFETY: The HANDLE is not thread-safe by type but is logically thread-safe in this case.
+    unsafe impl Sync for ThreadSafeCustomHeap {}
+
+    #[linked::object]
+    struct MemoryRegionSpecificHeap {
+        // Assigned when the thread first tries to allocate region-specific memory.
+        // We assume that the same thread will never move to another region in this case,
+        // otherwise, we will be sharing the heap across regions, which is not what we want.
+        current_region_heap: RefCell<Option<Arc<ThreadSafeCustomHeap>>>,
+
+        // This is our big bag of all the heaps. We create the heap on-demand,
+        // on a thread that is running in the target memory region.
+        #[allow(clippy::type_complexity)]
+        per_region_heaps: Arc<Mutex<Box<[Option<Arc<ThreadSafeCustomHeap>>]>>>,
+    }
+
+    impl MemoryRegionSpecificHeap {
+        pub fn new() -> Self {
+            let memory_region_count = HardwareInfo::current().max_memory_region_count();
+            let per_region_heaps = Arc::new(Mutex::new(
+                vec![None; memory_region_count].into_boxed_slice(),
+            ));
+
+            linked::new!(Self {
+                current_region_heap: RefCell::new(None),
+                per_region_heaps: Arc::clone(&per_region_heaps),
+            })
+        }
+
+        pub fn alloc(&self, size: usize) -> *mut std::ffi::c_void {
+            self.with_current_heap(|heap| heap.alloc(size))
+        }
+
+        pub fn free(&self, ptr: *mut std::ffi::c_void) {
+            self.with_current_heap(|heap| heap.free(ptr))
+        }
+
+        fn with_current_heap<F, R>(&self, f: F) -> R
+        where
+            F: FnOnce(&ThreadSafeCustomHeap) -> R,
+        {
+            let mut current_region_heap = self.current_region_heap.borrow_mut();
+
+            let heap = current_region_heap.get_or_insert_with(|| {
+                let region =
+                    HardwareTracker::with_current(|tracker| tracker.current_memory_region_id());
+
+                let mut heaps = self.per_region_heaps.lock().unwrap();
+
+                let heap = heaps[region as usize]
+                    .get_or_insert_with(|| Arc::new(ThreadSafeCustomHeap::new()));
+
+                Arc::clone(heap)
+            });
+
+            f(heap)
         }
     }
 }
