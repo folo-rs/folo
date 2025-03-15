7e955ed7a32c99f3a77cce83b26c8bf1c128697f
Sander Saares <sander@saares.eu>
2025-03-15 23:07:51 +0200

    Docs tidy

--- crates/many_cpus/src/pal/abstractions/platform.rs
+++ crates/many_cpus/src/pal/abstractions/platform.rs
@@ -5,7 +5,10 @@ use nonempty::NonEmpty;
 use crate::{MemoryRegionId, ProcessorId, pal::ProcessorFacade};
 
 pub(crate) trait Platform: Debug + Send + Sync + 'static {
-    /// Returns all currently available processors.
+    /// Returns all processors available to the current process.
+    ///
+    /// The returned set will exclude processors that are not active or are forbidden from
+    /// being used due to resource constraints enforced by the operating system.
     ///
     /// The returned collection of processors is sorted by the processor ID for ease of testing.
     /// TODO: Should we just sort in tests instead to avoid baking in some sorting assumptions?
@@ -20,18 +23,26 @@ pub(crate) trait Platform: Debug + Send + Sync + 'static {
 
     /// Gets the IDs of all processors that the current thread is allowed to execute on.
     ///
-    /// Note: this may be a superset of get_all_processors() - the final filtering is performed
-    /// by the latter, to avoid double-filtering in different parts of the workflow.
+    /// Note: this may be a superset of get_all_processors() because it may include processors
+    /// that our process is in fact forbidden to use due to resource constraints enforced by
+    /// the operating system. The filtering to only see what we are allowed to use is performed
+    /// by `get_all_processors()` but not by this function.
     fn current_thread_processors(&self) -> NonEmpty<ProcessorId>;
 
     /// Gets the maximum (inclusive) processor ID of any processor that could possibly
     /// be present on the system (including processors that are not currently active).
+    ///
+    /// The value also covers processors that are not available to the current process
+    /// due to resource constraints enforced by the operating system.
     ///
     /// This value is a constant and will not change over time.
     fn max_processor_id(&self) -> ProcessorId;
 
     /// Gets the maximum (inclusive) memory region ID of any memory region that could possibly
     /// be present on the system (including memory regions that are not currently active).
+    ///
+    /// The value also covers memory regions that are not available to the current process
+    /// due to resource constraints enforced by the operating system.
     ///
     /// This value is a constant and will not change over time.
     fn max_memory_region_id(&self) -> MemoryRegionId;
