38cc312d71d0d7c2e5d77be2f8f2006dee5dd290
Sander Saares <sander@saares.eu>
2025-07-23 22:26:04 +0300

    Fixx

--- packages/alloc_tracker/src/constants.rs
+++ packages/alloc_tracker/src/constants.rs
@@ -1,4 +1,4 @@
-//! Constants used throughout the alloc_tracker package.
+//! Constants used throughout the `alloc_tracker` package.
 
 // A poisoned lock means the process is in an unrecoverable/unsafe state and must exit (we panic).
 pub(crate) const ERR_POISONED_LOCK: &str = "encountered poisoned lock - continued execution is not safe because we can no longer ensure that we uphold security and privacy guarantees";
