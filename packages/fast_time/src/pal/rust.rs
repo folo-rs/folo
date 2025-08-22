f559d52dc4378b6872c5311c369121bfade91b9e
Sander Saares <sander@saares.eu>
2025-08-22 11:16:01 +0300

    Dependencies tidy + udeps

--- packages/fast_time/src/pal/rust.rs
+++ packages/fast_time/src/pal/rust.rs
@@ -4,7 +4,7 @@ use crate::pal::{Platform, TimeSource};
 
 /// We use this under Miri because Miri cannot talk to a real OS but Rust std time still works.
 #[derive(Debug)]
-#[cfg_attr(not(miri), expect(dead_code, reason = "conditional"))]
+#[allow(dead_code, reason = "conditional")]
 pub(crate) struct RustPlatform;
 
 impl Platform for RustPlatform {
