64d50d35c11ab880b2d20a39fbda1b9623b4e620
Sander Saares <sander@saares.eu>
2025-09-07 11:43:57 +0300

    Minor optimize

--- packages/infinity_pool/src/opaque/vacancy_tracker.rs
+++ packages/infinity_pool/src/opaque/vacancy_tracker.rs
@@ -59,11 +59,11 @@ impl VacancyTracker {
             if self.next_vacancy.is_none() {
                 self.next_vacancy = Some(previous_count);
             }
-        } else if let Some(current_next) = self.next_vacancy {
+        } else if let Some(next_vacancy) = self.next_vacancy {
             // If we removed slabs, and the current next vacancy is now out of range,
             // this means there are no more vacancies (because the current one was already
             // the lowest-index one).
-            if current_next >= count {
+            if next_vacancy >= count {
                 self.next_vacancy = None;
             }
         }
@@ -71,26 +71,43 @@ impl VacancyTracker {
 
     /// Updates the vacancy status of a slab.
     pub(crate) fn update_slab_status(&mut self, slab_index: usize, has_vacancy: bool) {
-        let previous = self.has_vacancy.replace(slab_index, has_vacancy);
+        let slab_previously_had_vacancy = self.has_vacancy.replace(slab_index, has_vacancy);
+
+        if has_vacancy == slab_previously_had_vacancy {
+            // No change.
+            return;
+        }
 
         if has_vacancy {
             // If we just added a vacancy, and it's the lowest-index vacancy, update our cache.
-            if !previous {
-                if let Some(current_next) = self.next_vacancy {
-                    if slab_index < current_next {
-                        self.next_vacancy = Some(slab_index);
-                    }
-                } else {
+            if let Some(next_vacancy) = self.next_vacancy {
+                if slab_index < next_vacancy {
                     self.next_vacancy = Some(slab_index);
                 }
+            } else {
+                self.next_vacancy = Some(slab_index);
             }
         } else {
             // If we just removed a vacancy, and it was the lowest-index vacancy, find the next one.
-            if previous {
-                if let Some(current_next) = self.next_vacancy {
-                    if slab_index == current_next {
-                        self.next_vacancy = self.has_vacancy.iter_ones().next();
-                    }
+            if let Some(next_vacancy) = self.next_vacancy {
+                if slab_index == next_vacancy {
+                    // There may be a vacancy in a later slab (but never earlier,
+                    // as we fill from the start of the slab list).
+                    //
+                    // Will not wrap because wrapping implies we have more slabs than virtual memory.
+                    let remaining_range_start = slab_index.wrapping_add(1);
+                    let remaining_range = remaining_range_start..;
+
+                    let Some(remaining_bits) = self.has_vacancy.get(remaining_range) else {
+                        // This was the last slab, so there are no more vacancies.
+                        self.next_vacancy = None;
+                        return;
+                    };
+
+                    // Will not wrap because wrapping implies we have more slabs than virtual memory.
+                    self.next_vacancy = remaining_bits.first_one().map(|index_in_remaining| {
+                        remaining_range_start.wrapping_add(index_in_remaining)
+                    });
                 }
             }
         }
