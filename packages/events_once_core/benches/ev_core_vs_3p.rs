9531ee29259eb95b39305f6610f00776b2902b7c
Sander Saares <sander@saares.eu>
2025-11-02 22:01:48 +0200

    2poll

--- packages/events_once_core/benches/ev_core_vs_3p.rs
+++ packages/events_once_core/benches/ev_core_vs_3p.rs
@@ -47,6 +47,45 @@ fn entrypoint(c: &mut Criterion) {
         });
     });
 
+    g.bench_function("local_boxed_send_receive_2poll", |b| {
+        b.iter(|| {
+            let (sender, receiver) = LocalEvent::<i32>::boxed();
+            let mut receiver = pin!(receiver);
+
+            let mut cx = task::Context::from_waker(Waker::noop());
+
+            _ = black_box(receiver.as_mut().poll(&mut cx));
+            sender.send(42);
+            _ = black_box(receiver.as_mut().poll(&mut cx));
+        });
+    });
+
+    g.bench_function("sync_boxed_send_receive_2poll", |b| {
+        b.iter(|| {
+            let (sender, receiver) = Event::<i32>::boxed();
+            let mut receiver = pin!(receiver);
+
+            let mut cx = task::Context::from_waker(Waker::noop());
+
+            _ = black_box(receiver.as_mut().poll(&mut cx));
+            sender.send(42);
+            _ = black_box(receiver.as_mut().poll(&mut cx));
+        });
+    });
+
+    g.bench_function("oneshot_send_receive_2poll", |b| {
+        b.iter(|| {
+            let (sender, receiver) = oneshot::channel::<i32>();
+            let mut receiver = pin!(receiver);
+
+            let mut cx = task::Context::from_waker(Waker::noop());
+
+            _ = black_box(receiver.as_mut().poll(&mut cx));
+            sender.send(42).unwrap();
+            _ = black_box(receiver.as_mut().poll(&mut cx));
+        });
+    });
+
     g.finish();
 }
 
