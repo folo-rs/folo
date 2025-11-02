c0baaccb671bb2c610a73945105f2319b5545003
Sander Saares <sander@saares.eu>
2025-11-02 21:58:28 +0200

    More bench

---
+++ packages/events_once_core/benches/ev_core_vs_3p.rs
@@ -0,0 +1,54 @@
+#![expect(missing_docs, reason = "benchmarks")]
+
+use std::hint::black_box;
+use std::pin::pin;
+use std::task;
+use std::task::Waker;
+
+use criterion::{Criterion, criterion_group, criterion_main};
+use events_once_core::{Event, LocalEvent};
+
+fn entrypoint(c: &mut Criterion) {
+    let mut g = c.benchmark_group("events_once_core_vs_3p");
+
+    g.bench_function("local_boxed_send_receive", |b| {
+        b.iter(|| {
+            let (sender, receiver) = LocalEvent::<i32>::boxed();
+            let mut receiver = pin!(receiver);
+
+            sender.send(42);
+
+            let mut cx = task::Context::from_waker(Waker::noop());
+            _ = black_box(receiver.as_mut().poll(&mut cx));
+        });
+    });
+
+    g.bench_function("sync_boxed_send_receive", |b| {
+        b.iter(|| {
+            let (sender, receiver) = Event::<i32>::boxed();
+            let mut receiver = pin!(receiver);
+
+            sender.send(42);
+
+            let mut cx = task::Context::from_waker(Waker::noop());
+            _ = black_box(receiver.as_mut().poll(&mut cx));
+        });
+    });
+
+    g.bench_function("oneshot_send_receive", |b| {
+        b.iter(|| {
+            let (sender, receiver) = oneshot::channel::<i32>();
+            let mut receiver = pin!(receiver);
+
+            sender.send(42).unwrap();
+
+            let mut cx = task::Context::from_waker(Waker::noop());
+            _ = black_box(receiver.as_mut().poll(&mut cx));
+        });
+    });
+
+    g.finish();
+}
+
+criterion_group!(benches, entrypoint);
+criterion_main!(benches);
