149067e31768aa1c479e2605ae0c5e7348593c0d
Sander Saares <sander@saares.eu>
2026-01-04 10:16:53 +0800

    Mark many_cpus_benchmarking as intentionally uncovered by tests

--- packages/many_cpus_benchmarking/src/lib.rs
+++ packages/many_cpus_benchmarking/src/lib.rs
@@ -1,4 +1,6 @@
 #![cfg_attr(coverage_nightly, feature(coverage_attribute))]
+// This is currently impractical to test as we lack the capability to simulate mock processor configurations.
+#![cfg_attr(coverage_nightly, coverage(off))]
 
 //! [Criterion][1] benchmark harness designed to compare different modes of distributing work in a
 //! many-processor system with multiple memory regions. This helps highlight the performance impact of
